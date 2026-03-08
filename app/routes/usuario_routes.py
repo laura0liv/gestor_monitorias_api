198ad3a51709473d0c7e58937fa90e406f86e5c9
laura0liv <laura.olivella.macias@gmail.com>
2026-03-08 12:58:11 -0500

    Agrego endpoints de usuario

--- app/routes/usuario_routes.py
+++ app/routes/usuario_routes.py
@@ -1,5 +1,6 @@
 
 from fastapi import APIRouter
+from fastapi.encoders import jsonable_encoder
 from controllers.usuario_controller import UsuarioController
 from models.usuario_model import Usuario
 
@@ -12,3 +13,24 @@ async def get_usuario():
     rpta = nuevo_usuario.get_all_usuario()
     return rpta
 
+@router.get("/get_usuario/{id_usuario}")
+async def get_usuario(id_usuario: int):
+    rpta = nuevo_usuario.get_usuario(id_usuario)
+    return rpta 
+
+@router.post("/create_usuario/")
+async def create_usuario(usuario: Usuario): 
+    usuario_data = jsonable_encoder(usuario)
+    rpta = nuevo_usuario.create_usuario(usuario_data)
+    return rpta
+
+@router.put("/update_usuario/{id_usuario}")
+async def update_usuario(id_usuario: int, usuario: Usuario):
+    usuario_data = jsonable_encoder(usuario)
+    rpta = nuevo_usuario.update_usuario(id_usuario, usuario_data)
+    return rpta
+
+@router.delete("/delete_usuario/{id_usuario}")
+async def delete_usuario(id_usuario: int):
+    rpta = nuevo_usuario.delete_usuario(id_usuario)
+    return rpta
